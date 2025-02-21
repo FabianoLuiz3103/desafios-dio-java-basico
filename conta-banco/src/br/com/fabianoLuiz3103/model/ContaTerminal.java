74d657d07075d1cc72939eb39918e7b151fed3c2
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-21 18:54:49 -0300

    refactor: adicionando java doc na classe ContaTerminal

--- conta-banco/src/br/com/fabianoLuiz3103/model/ContaTerminal.java
+++ conta-banco/src/br/com/fabianoLuiz3103/model/ContaTerminal.java
@@ -1,5 +1,13 @@
 package br.com.fabianoLuiz3103.model;
 
+/**
+ * <h1>Conta terminal</h1>
+ * A classe conta terminal realiza a criação de contas
+ *
+ * @author FabianoLuiz3103
+ * @version 1.0
+ * @since 21/02/2025
+ */
 public class ContaTerminal {
 
     private int numero;
@@ -10,6 +18,13 @@ public class ContaTerminal {
     public ContaTerminal() {
     }
 
+    /**
+     *
+     * @param numero é o numéro da conta do cliente
+     * @param agencia é o número da agência do cliente
+     * @param nomeCliente é o nome completo do cliente
+     * @param saldo é o saldo inicial da conta do cliente
+     */
     public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
         this.numero = numero;
         this.agencia = agencia;
@@ -20,7 +35,7 @@ public class ContaTerminal {
     @Override
     public String toString() {
         return "Olá ".concat(this.nomeCliente).concat(", obrigado por criar uma conta" +
-                "em nosso banco, sua agência é ").concat(this.agencia).concat(", conta ")
+                " em nosso banco, sua agência é ").concat(this.agencia).concat(", conta ")
                 .concat(String.valueOf(this.numero)).concat(" e seu saldo de R$")
                 .concat(String.valueOf(this.saldo)).concat(" já está disponível para saque!");
     }
