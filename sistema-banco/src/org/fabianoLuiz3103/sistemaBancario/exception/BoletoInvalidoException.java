4e22573a23badcef227482af3be851c303f6beb6
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-27 18:56:33 -0300

    feat: criando exception para tentativa de pagamento de boleto com código inválido

---
+++ sistema-banco/src/org/fabianoLuiz3103/sistemaBancario/exception/BoletoInvalidoException.java
@@ -0,0 +1,7 @@
+package org.fabianoLuiz3103.sistemaBancario.exception;
+
+public class BoletoInvalidoException extends Exception{
+    public BoletoInvalidoException(String message) {
+        super(message);
+    }
+}
