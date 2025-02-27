96eb4f30c9f440c281044a5b51cb32788b9490bf
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-27 18:55:46 -0300

    feat: criando exception para tentativa de saque sem saldo

---
+++ sistema-banco/src/org/fabianoLuiz3103/sistemaBancario/exception/SemSaldoException.java
@@ -0,0 +1,7 @@
+package org.fabianoLuiz3103.sistemaBancario.exception;
+
+public class SemSaldoException extends Exception{
+    public SemSaldoException(String message) {
+        super(message);
+    }
+}
