93bc0d30157a1ea0ad62315ec7138b4ba24dce9b
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-27 18:56:06 -0300

    feat: criando exception para tentativa de depósito de valor negativo

---
+++ sistema-banco/src/org/fabianoLuiz3103/sistemaBancario/exception/DepositoInvalidoException.java
@@ -0,0 +1,7 @@
+package org.fabianoLuiz3103.sistemaBancario.exception;
+
+public class DepositoInvalidoException extends Exception{
+    public DepositoInvalidoException(String message) {
+        super(message);
+    }
+}
