94b83c1edd3297a313beafbcec09889a5b027795
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-02-27 21:46:35 -0300

    feat: criando exceções de velocidade inválida e veiculo desligado

---
+++ controle-veiculo/src/org/fabianoLuiz3103/controleVeiculo/exception/VeiculoDesligadoExcpetion.java
@@ -0,0 +1,7 @@
+package org.fabianoLuiz3103.controleVeiculo.exception;
+
+public class VeiculoDesligadoExcpetion extends Exception {
+    public VeiculoDesligadoExcpetion(String message) {
+        super(message);
+    }
+}
---
+++ controle-veiculo/src/org/fabianoLuiz3103/controleVeiculo/exception/VelocidadeInvalidaException.java
@@ -0,0 +1,7 @@
+package org.fabianoLuiz3103.controleVeiculo.exception;
+
+public class VelocidadeInvalidaException extends Exception{
+    public VelocidadeInvalidaException(String message) {
+        super(message);
+    }
+}
